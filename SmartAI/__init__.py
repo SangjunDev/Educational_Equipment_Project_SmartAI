721b51fad6de1a7abd1443c47d9d62f275afa4fa
SnagjunDev <sangjun.kim.dev@gmail.com>
2022-05-30 11:12:40 +0900

    Rename(control.py -> actuator_control.py)

--- SmartAI/__init__.py
+++ SmartAI/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template 
-from . import control 
+from . import actuator_control 
 from . import read_db
 from . import index
 
 app = Flask(__name__)
 
-app.register_blueprint(control.blue_control)
+app.register_blueprint(actuator_control.blue_control)
 app.register_blueprint(read_db.blue_read)
 app.register_blueprint(index.blue_index)
  
